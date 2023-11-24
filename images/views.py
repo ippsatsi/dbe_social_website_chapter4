b1a37d3c24a358f5e5a4c37bef10114388010840
Luis Aguilar <laz133@example.com>
2023-11-23 20:16:15 -0500

    Implementando formulario para marcar imagenes

--- images/views.py
+++ images/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
 
-# Create your views here.
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        # formulario es enviado
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            # datos del formulario son validos
+            cd = form.cleaned_data
+            new_image = form.save(commit=False)
+            # asignamos el usuario actual al item
+            new_image.user = request.user
+            new_image.save()
+            messages.success(request,
+                             'Imagen se agregó con éxito')
+            # redireccionamos a la vista del item recientemente creado
+            return redirect(new_image.get_absolute_url())
+        
+    else:
+        # construir formulario con data obtenida del bookmarklet
+        # via GET
+        form = ImageCreateForm(data=request.GET)
+    return render(request,
+                  'images/image/create.html',
+                  {'section': 'images',
+                   'form': form})
